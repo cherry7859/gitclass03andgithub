ef0e962676b0214ce0fb2fa315e9722ba79f0866
cherry7859 <cmfinley97@gmail.com>
2022-05-31 18:54:22 -0500

    2nd commit

--- src/github/GitAndGithub.java
+++ src/github/GitAndGithub.java
@@ -4,7 +4,9 @@ public class GitAndGithub {
 
 	public static void main(String[] args) {
 		
-		System.out.print("today we learn github");
+		System.out.print("Today we learn github");
+		System.out.print("We are making a change");
+		System.out.println("Today is a good day");
 		
 	}
 }
